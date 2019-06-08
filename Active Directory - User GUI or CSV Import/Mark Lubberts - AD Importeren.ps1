543fad2e0803ac64e502b265f60c6ea5d666575a
Mark Lubberts <m.lubberts02@gmail.com>
2019-06-08 17:55:38 +0200

    Fix minor bug considering account validation, the bug being a lack of such validation.
    Signed-off-by: Mark Lubberts <m.lubberts02@gmail.com>

--- Active Directory - User GUI or CSV Import/Mark Lubberts - AD Importeren.ps1
+++ Active Directory - User GUI or CSV Import/Mark Lubberts - AD Importeren.ps1
@@ -32,8 +32,13 @@ Function MaakGebruikerAan {
     $gebr_unaam = "$gebr_filter.$gebr_Achternaam".ToLower()
 
     # Maak de gebruiker zelf aan met alle gegevens en een geforceerde password reset.
+    # We doen ook direct een controle of het account bestaat en geven de juiste melding door!
     New-ADUser -Name "$gebr_Voornaam $gebr_Achternaam" -GivenName $gebr_Voornaam -Surname $gebr_Achternaam -UserPrincipalName $gebr_unaam -OfficePhone $gebr_telnr -EmailAddress $gebr_Email -Description $gebr_Functie -AccountPassword $gebr_Password -Path $gebr_OUPad -ChangePasswordAtLogon $True -Enabled $True
-    $txtb_Output.Text = $txtb_Output.Text + "[AD_USR]: $gebr_Voornaam $gebr_Achternaam is aangemaakt!`r`n"
+    if(@(Get-ADUser -Filter { SamAccountName -eq $gebr_unaam }).Count -eq 0) {  
+        $txtb_Output.Text = $txtb_Output.Text + "[AD_USR]: Het account '$gebr_Voornaam $gebr_Achternaam' kon niet worden aangemaakt!`r`n"
+    } catch {    
+        $txtb_Output.Text = $txtb_Output.Text + "[AD_USR]: $gebr_Voornaam $gebr_Achternaam is aangemaakt!`r`n"
+    }
 }
 
 # Deze functie leest een Csv bestand uit en importeert deze naar de MaakGebruikerAan functie.
